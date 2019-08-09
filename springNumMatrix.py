6bdf420888008056a6e11136ab345a42b7b32bb0
Unknown <cesar.garza.826@gmail.com>
2019-08-08 23:13:40 -0500

    more updates

--- springNumMatrix.py
+++ springNumMatrix.py
@@ -20,21 +20,35 @@ def generateMatrix(size):
     mat.append([*[0]*2*(size - 1), *lastLine])
     return np.array(mat)
 
+def generateMatrix2(li):
+    mat = []
+    li.append('q')
+    for i,x in enumerate(li):
+        if i is 0:
+            mat.append([x, -1 * x, *[0]*(len(li)-2)])
+        elif i is (len(li) - 1):
+            mat.append([*[0]*(len(li)-2),-1 * li[i - 1],li[i - 1]])
+        else:
+            last = li[i - 1]
+            mat.append([*[0]*(i-1), -1 * last, last + x, -1 * x, *[0]*(len(li) - 2 - i)])
+    return np.array(mat)
+
 def findLambdaPrime(ar):
     ar -= k + kappa
     ar = ar ** 2
     ar -= k ** 2 + kappa ** 2
     ar /= k * kappa
-    return ar  
+    return ar 
 
-trivCase = np.array([firstLine, lastLine])
-eig = np.linalg.eigvals(trivCase)
-print(eig)
-print(findLambdaPrime(eig))
+# trivCase = np.array([firstLine, lastLine])
+# eig = np.linalg.eigvals(trivCase)
+# print(eig)
+# print(findLambdaPrime(eig))
 
-# threeCase = generateMatrix(3)
-# print('threeCase')
-# print(np.linalg.eigvals(threeCase))
+threeCase = generateMatrix(3)
+print(threeCase)
+print('threeCase')
+print(np.linalg.eigvals(threeCase))
 
 # fourCase = generateMatrix(4)
 # print('fourCase')
@@ -49,16 +63,22 @@ print(findLambdaPrime(eig))
 # eig = np.linalg.eigvals(sixCase)
 # print(findLambdaPrime(eig))
 
-sevenCase = generateMatrix(7)
-print('sevenCase')
-eig = np.linalg.eigvalsh(sevenCase.astype(np.float64))
-print(findLambdaPrime(eig))
+# sevenCase = generateMatrix(7)
+# print('sevenCase')
+# eig = np.linalg.eigvalsh(sevenCase.astype(np.float64))
+# print(findLambdaPrime(eig))
 
 # eightCase = generateMatrix(8)
 # print('eightCase')
 # print(np.linalg.eigvals(eightCase))
 
-tenCase = generateMatrix(10)
-print('tenCase')
-eig = np.linalg.eigvalsh(tenCase.astype(np.float64))
-print(findLambdaPrime(eig))
+# tenCase = generateMatrix(10)
+# print('tenCase')
+# eig = np.linalg.eigvalsh(tenCase.astype(np.float64))
+# print(findLambdaPrime(eig))
+
+eightMasses = generateMatrix2([2,3,2,3,2,3,2,3])
+print(eightMasses)
+print(np.linalg.eigvals(eightMasses))
+print(k + kappa + np.sqrt(k ** 2 + kappa ** 2 - 2 * k * kappa * np.cos(np.array([np.pi / 9, np.pi * 3 / 9, np.pi * 5 / 9, np.pi * 7/9]))))
+print(k + kappa - np.sqrt(k ** 2 + kappa ** 2 - 2 * k * kappa * np.cos(np.array([np.pi / 9, np.pi * 3 / 9, np.pi * 5 / 9, np.pi * 7/9]))))
